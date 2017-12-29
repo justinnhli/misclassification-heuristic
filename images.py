814784368bbbbb791fb7c08edeedcbb7fe89295c
Justin Li <justinnhli@gmail.com>
2017-12-29 14:59:43 -0800

    fix command line arguments

--- images.py
+++ images.py
@@ -306,8 +306,7 @@ def main():
         int_labels = sample_classes(10, args.num_labels)
     elif args.dataset == 'cifar100':
         int_labels = sample_classes(100, args.num_labels)
-    print(args, len(int_labels))
-    #train_neural_network(int_labels, args.batch, args.epochs, args.dataset, verbose=args.verbose)
+    train_neural_network(int_labels, args.batch, args.epochs, args.dataset, verbose=args.verbose)
 
 
 if __name__ == '__main__':
