8a3a9ef297a81c7cfd3a5978fa33ea7662d81de7
Justin Li <justinnhli@gmail.com>
2018-06-05 18:31:31 -0700

    activate venv in cifar10-all run

--- run-cifar10-all.sh
+++ run-cifar10-all.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-clusterun.py --labels="$(./powerset.py)" './run-cifar10-all.py $labels'
+read -r -d '' commands << EOM
+source "/home/justinnhli/.venv/misclassification-heuristic/bin/activate"
+./run-cifar10-all.py \$labels
+deactivate
+EOM
+clusterun.py --labels="$(./powerset.py)" "$commands"
